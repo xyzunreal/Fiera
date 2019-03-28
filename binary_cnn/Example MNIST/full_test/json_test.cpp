258182d8f12c88ccfbbacda92f899b249d338955
Deepak Mangla <dmangla3@gmail.com>
2019-03-28 16:03:09 +0530

    Added Json library

---
+++ binary_cnn/Example MNIST/full_test/json_test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "json.hpp"
+using namespace std;
+using json = nlohmann::json;
+int main() {
+json j;
+
+// add a number that is stored as double (note the implicit conversion of j to an object)
+j["pi"] = 3.141;
+
+// add a Boolean that is stored as bool
+j["happy"] = true;
+
+// add a string that is stored as std::string
+j["name"] = "Niels";
+
+// add another null object by passing nullptr
+j["nothing"] = nullptr;
+
+// add an object inside the object
+j["answer"]["everything"] = 42;
+
+// add an array that is stored as std::vector (using an initializer list)
+j["list"] = { 1, 0, 2 };
+
+// add another object (using an initializer list of pairs)
+j["object"] = { {"currency", "USD"}, {"value", 42.99} };
+
+j["binary"] = {true,false,true};
+
+cout<<j;
+// instead, you could also write (which looks very similar to the JSON above)
+json j2 = {
+  {"pi", 3.141},
+  {"happy", true},
+  {"name", "Niels"},
+  {"nothing", nullptr},
+  {"answer", {
+    {"everything", 42}
+  }},
+  {"list", {1, 0, 2}},
+  {"object", {
+    {"currency", "USD"},
+    {"value", 42.99}
+  }}
+};
+cout<<j2;
+        return 0;
+}
+
